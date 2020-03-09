7978c1a35a9bd5e0ed1c01c5db622ae1d4c4720d
Tobias Waldekranz <tobias@waldekranz.com>
2020-03-09 23:28:15 +0100

    test: add quantize test

--- test/test.sh
+++ test/test.sh
@@ -24,6 +24,7 @@ case=if-stmt && ply_simple 'if (pid > 1) exit(0); else exit(1);' && \
 case=print && ply_simple 'print("test"); exit(0);' && \
     [ $stdout = test ] || fail test "$stdout"
 
+
 case=wildcard
 ply -c \
     "dd if=/dev/zero of=/dev/null bs=1 count=100" \
@@ -34,5 +35,16 @@ cat /tmp/wildcard | awk '
     /dd.*vfs_write/ { if ($NF > 100) write = 1; }
     END             { exit(!(read && write)); }' \
 || fail "at least 100 reads/writes" "$(cat /tmp/wildcard)"
+
+
+case=quantize
+ply -c \
+    "dd if=/dev/zero of=/dev/null bs=10240 count=10" \
+    'kr:vfs_read if (!strcmp(comm, "dd")) {
+    		 @["rdsz"] = quantize(retval);
+     }' >/tmp/quantize \
+&& \
+grep -qe '8k\s*,\s*16k\s*)\s*10' /tmp/quantize \
+|| fail "10 reads in (8k, 16k]" "$(cat /tmp/quantize)"
 
 exit $total_fails
