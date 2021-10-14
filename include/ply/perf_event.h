d9ffbfaa5e232385c6d0794b2955e0236c3fd4d0
Namhyung Kim <namhyung@gmail.com>
2021-10-14 00:29:17 -0700

    aux: add perf_event_attach_raw() function
    The perf_event_attach_raw() does similar to perf_event_attach() but it
    allows to pass raw event info like type, config and period.  This will
    be used to create custom event like in the interval timer.

--- include/ply/perf_event.h
+++ include/ply/perf_event.h
@@ -13,6 +13,8 @@ struct ply_probe;
 
 int perf_event_attach(struct ply_probe *pb, const char *name,
 		      int task_mode);
+int perf_event_attach_raw(struct ply_probe *pb, int type, unsigned long config,
+			  unsigned long long period, int task_mode);
 
 int perf_event_enable (int group_fd);
 int perf_event_disable(int group_fd);
--- src/libply/aux/perf_event.c
+++ src/libply/aux/perf_event.c
@@ -57,10 +57,36 @@ int perf_event_attach(struct ply_probe *pb, const char *path,
 	if (fd < 0)
 		return -errno;
 
-	/* if (ioctl(fd, PERF_EVENT_IOC_ENABLE, 0)) { */
-	/* 	close(fd); */
-	/* 	return -errno; */
-	/* } */
+	if (ioctl(fd, PERF_EVENT_IOC_SET_BPF, pb->bpf_fd)) {
+		close(fd);
+		return -errno;
+	}
+
+	if (pb->ply->group_fd == -1 && !task_mode)
+		pb->ply->group_fd = fd;
+
+	return fd;
+}
+
+int perf_event_attach_raw(struct ply_probe *pb, int type, unsigned long config,
+			  unsigned long long period, int task_mode)
+{
+	struct perf_event_attr attr = {};
+	int fd;
+	int pid = task_mode ? 0 : -1;
+	int cpu = task_mode ? -1 : 0;
+	int group_fd = task_mode ? -1 : pb->ply->group_fd;
+
+	attr.type = type;
+	attr.config = config;
+	attr.sample_type = PERF_SAMPLE_RAW;
+	attr.sample_period = period;
+	attr.wakeup_events = 1;
+	attr.disabled = 1;
+
+	fd = perf_event_open(&attr, pid, cpu, group_fd, 0);
+	if (fd < 0)
+		return -errno;
 
 	if (ioctl(fd, PERF_EVENT_IOC_SET_BPF, pb->bpf_fd)) {
 		close(fd);
